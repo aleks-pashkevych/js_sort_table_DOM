bc0220637138c0d0400be1cd81c0ae2ec186cf20
Aleks Pashkevych <alekspashkevych@gmail.com>
2026-01-14 10:08:10 +0200

    task looks working, test fails

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,43 @@
 'use strict';
 
 // write code here
+const tHead = document.querySelector('thead');
+const tBody = document.querySelector('tbody');
+const heads = tHead.querySelectorAll('th');
+const rows = Array.from(tBody.querySelectorAll('tr'));
+
+const sortRows = (column) =>
+  rows.sort((a, b) => {
+    const trimVal = (val) => {
+      let value = val
+        .querySelector(`td:nth-child(${column + 1})`)
+        .textContent.trim();
+
+      if (value[0] === '$') {
+        value = Number(value.replace(/[^0-9.-]+/g, ''));
+      }
+
+      return value;
+    };
+    const aTrimmed = trimVal(a);
+    const bTrimmed = trimVal(b);
+
+    return aTrimmed > bTrimmed;
+  });
+
+const rebuildTable = (column) => {
+  const sortedRows = sortRows(column);
+
+  while (tBody.firstChild) {
+    tBody.removeChild(tBody.firstChild);
+  }
+  tBody.append(...sortedRows);
+};
+
+rebuildTable(0);
+
+for (const cell of heads) {
+  cell.addEventListener('click', (e) => {
+    rebuildTable(e.target.cellIndex);
+  });
+}
